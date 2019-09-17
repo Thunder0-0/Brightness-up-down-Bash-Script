20a2691452f8b2ec2e79fc95e421a0910b05623a
Thunder0-0 <spocra.0@gmail.com>
2019-09-17 20:13:42 +0300

    add scripts

---
+++ bright_up.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function bright(){
+	read bright < /sys/class/backlight/acpi_video0/brightness
+	echo $((bright+(1))) | sudo tee /sys/class/backlight/acpi_video0/brightness
+}
+
+DIR=$( cd "$( dirname "$0" )" && pwd )   		# get script dir
+if [ "$UID" -ne 0 ]; then                		# check if you are root
+    	Password="Qasxcv123456"
+	echo $Password |sudo -S $DIR/bright_up.sh	# NOT: run script with sudo
+else
+    bright                                  		# YES: run function
+fi
